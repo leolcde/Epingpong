79b974e9ee66a9017f9eb2bbcede654fdff1d4db
Léo Lacordaire <leo.lacordaire@epitech.eu>
2025-03-07 17:28:22 +0100

    [bot] init bot

--- src/bot.py
+++ src/bot.py
@@ -2,14 +2,12 @@ import discord
 
 client = discord.Client(intents=discord.Intents.default())
 
-client.run("MTM0NzU4NjUzNDUyMTQzODI1Mg.GCNp-R.9olsKlnOxowvvnR4f7HoQYhrxkXtyb5KPCVykU")
-
 @client.event
 async def on_ready():
     print("Le bot est prêt !")
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
     print(message.content)
+
+# put token here .
