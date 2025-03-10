7f66a161eb806786b399446f99e22530f90b6759
Léo Lacordaire <leo.lacordaire@epitech.eu>
2025-03-10 19:44:36 +0100

    [Epingpong] add join

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Epingpong
+
+### Discord PingPong ranking bot
--- src/bot.py
+++ src/bot.py
@@ -1,13 +1,26 @@
 import discord
 
-client = discord.Client(intents=discord.Intents.default())
+discord_token = "MTM0NzU4NjUzNDUyMTQzODI1Mg.GCNp-R.9olsKlnOxowvvnR4f7HoQYhrxkXtyb5KPCVykU"
+import discord
+
+default_intents = discord.Intents.default()
+client = discord.Client(intents=default_intents)
+
+match_queue = []
 
 @client.event
 async def on_ready():
-    print("Le bot est prêt !")
+    print("le bot est carré mgl")
 
 @client.event
 async def on_message(message):
-    print(message.content)
+    if message.author == client.user:
+        return
+    if message.content == "!join":
+        if message.author not in match_queue:
+            match_queue.append(message.author)
+            await message.channel.send(f"{message.author.mention} a rejoint la file d'attente. Position : {len(match_queue)}")
+        else:
+            await message.channel.send(f"{message.author.mention}, tu es déjà dans la file d'attente.")
 
-# put token here .
+client.run(discord_token)
