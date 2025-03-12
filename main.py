54931800c0c70ceddd98de5524eb289b736ac050
Bassirou Tall <bassirou.tall@epitech.eu>
2025-03-12 12:56:02 +0100

    build: ranking branch. feat: build player and player_elo class. build write on_json, check_already_played and check_players functions

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 import discord
+import json
 
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
-match_queue = []
+class player_elo:
+    def __init__(self, player_id, elo):
+        self.id = player_id
+        self.elo = elo
 
-@client.event
-async def on_ready():
-    print("Le bot est carré mgl")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+class player:
+    discord_token = "MTM0NzU4NjUzNDUyMTQzODI1Mg.GHLPFR.j40gTP0ko0OVaYLgpYmIb6GaW-1foTaYOBLB-Y"
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+    match_queue = []
+    @client.event
+    async def on_ready():
+        print("Le bot est carré mgl")
+    @client.event
+    async def on_message(message):
+        if message.author == player.client.user:
+            return
 
     # FOR JOIN THE QUEUE
     if message.content == "!join":
         user_id = message.author.id
-        if user_id not in match_queue:
-            match_queue.append(user_id)
-            await message.channel.send(f"{message.author.mention} a rejoint la file d'attente. Position : {len(match_queue)}")
+        if user_id not in player.match_queue:
+            player.match_queue.append(user_id)
+            await message.channel.send(f"{message.author.mention} a rejoint la file d'attente. Position : {len(player.match_queue)}")
         else:
             await message.channel.send(f"{message.author.mention}, tu es déjà dans la file d'attente.")
 
     # FOR DISPLAY THE QUEUE
     if message.content == "!queue":
-        if match_queue:
-            print(match_queue)
+        if player.match_queue:
+            print(player.match_queue)
             queue_list = []
-            for i, user_id in enumerate(match_queue):
-                user = await client.fetch_user(user_id)
+            for i, user_id in enumerate(player.match_queue):
+                user = await player.client.fetch_user(user_id)
                 if user is not None:
                     queue_list.append(f"{i+1}. {user.name}")
                 else:
@@ -41,12 +47,36 @@ async def on_message(message):
     # FOR LEAVE THE QUEUE
     if message.content == "!leave":
         i = 0
-        user_id = message.content
-        if user_id in match_queue:
-            while user_id != match_queue[i]:
+        user_id = message.author.id
+        if user_id in player.match_queue:
+            while user_id != player.match_queue[i]:
                 i += 1
-            match_queue.pop(i)
-            await message.channel.send(f"{message.autor.mention} a quitté la file d'attente.")
+            player.match_queue.pop(i)
+            await message.channel.send(f"{message.author.mention} a quitté la file d'attente.")
         else:
-            await message.channel.send(f"{message.autor.mention}, tu n'es pas dans la file d'attente.")
-client.run(discord_token)
+            await message.channel.send(f"{message.author.mention}, tu n'es pas dans la file d'attente.")
+
+def write_on_json(variable):
+    dictionary = {variable.id: variable.elo}
+    json_object = json.dumps(dictionary, indent=1)
+    with open("elo_data.json", "w") as outfile:
+        outfile.write(json_object)
+    return 0
+
+def check_already_played(id):
+    i = 0
+    with open("elo_data.json", "r") as openfile:
+        json_object = json.load(openfile)
+        if json_object.values(i) == id:
+            return 0
+        i += 1
+    return 84
+
+def check_players():
+    user = player_elo()
+    if check_already_played(player.message.author.id) == 84:
+        user = (player.message.author.id, 100)
+        write_on_json(user)
+    return 0
+
+player.client.run(player.discord_token)
